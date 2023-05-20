95cb7ad3a6ce27723985efc9a15f99a15ef20d17
kartik sen <karthiksen10@gmail.com>
2023-05-20 17:18:59 +0530

    some files deleted

--- src/componants/TodoList.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function TodoList(props) {
-
-
-
-  return (<> <li className="text-purple-600">  <i className= {`fa fa-times fa-circle-xmark text-purple-600 hover:text-red-500 ` } aria-hidden="true" onClick={()=>{props.onSelect(props.id); }}></i> {props.text}</li> </> )
-}
